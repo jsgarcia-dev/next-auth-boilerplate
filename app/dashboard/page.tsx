caa3cdb5ee76f5cfb18f84f0c20f696a7ad17a00
Julio Garcia <jsgarcia.dev@gmail.com>
2024-09-24 22:58:45 -0300

    chore: setup inicial

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,7 @@
-import { NewVerificationForm } from "@/components/form/verify-token-form";
-
 export default function DashboardPage() {
   return (
     <div className="pt-20 px-20">
       <h1 className="text-3xl font-semibold">Dashboard</h1>
-      <NewVerificationForm
-        data={{
-          success: true,
-          code: 200,
-          message: "fdsfdfsd",
-        }}
-      />
     </div>
   );
 }
